9e0d791575e1c1fe7bb1d3d812ac04d685b71bbe
MakLo1337 <mrwinataa@gmail.com>
2022-10-22 00:45:52 +0700

    first commit

---
+++ lib/services/cirestsmtpservices.dart
@@ -0,0 +1,16 @@
+part of 'services.dart';
+
+class Cirestsmtpservices {
+
+  static Future<http.Response> sendMail(String email){
+    return http.post(Uri.https(Const.smtpUrl, "/cirestapi/api/mahasiswa/sendmail"),
+      headers: <String, String> {
+        'content-type' : 'application/json; charset=utf-8'
+      },
+      body: jsonEncode(<String, dynamic>{
+        'email' : email
+      })
+    );
+  }
+
+}
---
+++ lib/services/services.dart
@@ -0,0 +1,6 @@
+import 'dart:convert';
+
+import 'package:email_cirest/shared/shared.dart';
+import 'package:http/http.dart' as http;
+
+part 'cirestsmtpservices.dart';
